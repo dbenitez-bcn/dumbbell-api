b37b42bdb16fa146dc32fde04e1d3ffb23765252
David Benitez Riba <dbr.bcn@gmail.com>
2019-12-30 12:56:45 +0100

    Step3

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,21 @@ import express from "express";
 import { applyMiddleware, applyRoutes } from "./utils";
 import routes from "./services/exercices/routes";
 import middleware from "./middleware";
+import errorHandlers from "./middleware/errorHandlers";
+
+process.on("uncaughtException", e => {
+  console.log(e);
+  process.exit(1);
+});
+process.on("unhandledRejection", e => {
+  console.log(e);
+  process.exit(1);
+});
 
 const router = express();
-applyMiddleware(middleware, router)
-applyRoutes(routes, router)
+applyMiddleware(middleware, router);
+applyRoutes(routes, router);
+applyMiddleware(errorHandlers, router);
 
 const { PORT = 3000 } = process.env;
 const server = http.createServer(router);
