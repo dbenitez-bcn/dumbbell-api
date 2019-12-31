c10c6348ac3de19eed5755337d947d7b483be957
David Benitez Riba <dbr.bcn@gmail.com>
2019-12-31 17:14:25 +0100

    migrations

---
+++ src/config/migrations.sql
@@ -0,0 +1,5 @@
+-- create database
+CREATE DATABASE dumbbell;
+-- \c dumbbell
+-- creates exercices table
+CREATE TABLE exercices (id SERIAL PRIMARY KEY, name VARCHAR);
--- src/services/exercices/providers/exercicesProvider.ts
+++ src/services/exercices/providers/exercicesProvider.ts
@@ -2,7 +2,7 @@ import { QueryResult } from "pg";
 import { database } from "../../../config/database";
 
 export const getExercicesFromDb = async () => {
-    const response: QueryResult = await database.query('SELECT * FROM planets');
+    const response: QueryResult = await database.query(`SELECT * FROM ${process.env.DB_TB_EXERCICES}`);
 
     return response.rows;
 }
