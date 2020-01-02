27fd233c48566e80451b2a663eeba3b74f5ec14c
David Benitez Riba <dbr.bcn@gmail.com>
2020-01-02 12:55:38 +0100

    Error handleling added

--- src/services/exercises/exercisesController.ts
+++ src/services/exercises/exercisesController.ts
@@ -2,15 +2,32 @@ import { Request, Response } from "express";
 import { getExercises, createExercise } from './providers/exercisesProvider'
 
 export const getExercisesHandler = async (req: Request, res: Response) => {
-  const result = await getExercises();
-  res.status(200).send(result);
-  // TODO: Add 404
+  try {
+    const result = await getExercises();
+    if (result.length > 0) res.status(200).send(result);
+    else res.status(404).send("No exercises found");
+
+  } catch (e) {
+    databaseErrorHandler(res);
+  }
 }
 
 export const createExerciseHandler = async (req: Request, res: Response) => {
   const name = req.body.name
   const description = req.body.description
-  const result = await createExercise(name, description);
-  res.status(201).send(result);
-  // TODO: Add 500, failure exeptions handler
+  if (name != null && description != null) {
+    try {
+      const result = await createExercise(name, description);
+      res.status(201).send(result);
+    } catch (e) {
+      databaseErrorHandler(res);
+    }
+  } else {
+    res.status(422).send("Missing params");
+  }
+}
+
+function databaseErrorHandler(res: Response) {
+  // TODO: Add logger to log the error message
+  res.status(500).send("A problem has occurred when accessing to database");
 }
