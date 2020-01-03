8f56a7d1a0de879f43bb09f49f8149c9f3994377
David Benitez Riba <dbr.bcn@gmail.com>
2020-01-03 20:41:32 +0100

    adds DumbbellDatabase class

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,7 +3,24 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const database: Pool = new Pool({
+export class DumbbellDatabase {
+    private static instance: DumbbellDatabase;
+
+    private constructor(
+        private _database: Pool
+    ) { }
+
+    static database(): Pool {
+        if (this.instance != null) return this.instance._database;
+        else return new DumbbellDatabase(defaultDatabase)._database;
+    }
+
+    static buildMockDatabase(database: Pool) {
+        this.instance = new DumbbellDatabase(database);
+    }
+}
+
+const defaultDatabase: Pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
--- src/services/exercises/providers/exercisesProvider.ts
+++ src/services/exercises/providers/exercisesProvider.ts
@@ -1,12 +1,12 @@
 import { QueryResult } from "pg";
-import { database } from "../../../config/database";
+import { DumbbellDatabase } from "../../../config/database";
 
 export const getExercises = async () => {
-    const response: QueryResult = await database.query(`SELECT * FROM ${process.env.DB_TB_EXERCISES}  ORDER BY id ASC`);
+    const response: QueryResult = await DumbbellDatabase.database().query(`SELECT * FROM ${process.env.DB_TB_EXERCISES}  ORDER BY id ASC`);
     return response.rows;
 }
 
 export const createExercise = async (name: String, description: String) => {
-    const response: QueryResult = await database.query(`INSERT INTO ${process.env.DB_TB_EXERCISES} (name, description) VALUES ('${name}', '${description}') RETURNING *`);
+    const response: QueryResult = await DumbbellDatabase.database().query(`INSERT INTO ${process.env.DB_TB_EXERCISES} (name, description) VALUES ('${name}', '${description}') RETURNING *`);
     return response.rows[0];
 }
