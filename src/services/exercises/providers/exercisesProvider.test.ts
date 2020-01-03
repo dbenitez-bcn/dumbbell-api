847be614800c83a34f813567e25770b583f2dfb6
David Benitez Riba <dbr.bcn@gmail.com>
2020-01-03 22:13:22 +0100

    unit test working

---
+++ src/services/exercises/providers/exercisesProvider.test.ts
@@ -0,0 +1,28 @@
+import { Pool } from "pg";
+import { when, mock, anyString, instance } from "ts-mockito";
+import { DumbbellDatabase } from "../../../config/database";
+import { getExercises, createExercise } from "./exercisesProvider";
+
+beforeEach(setUpDatabaseMock);
+
+describe("ExerciseProvider", () => {
+    describe("Get all exercises", () => {
+        test("Should return all exercises", async () => {
+            const result = await getExercises();
+            expect(result).toEqual(["testResult"]);
+        });
+    });
+    describe("Create exercise", () => {
+        test("Should return the created exercise", async () => {
+            const result = await createExercise("testName", "testDescription");
+            expect(result).toBe("testResult");
+        });
+    });
+});
+
+function setUpDatabaseMock() {
+    const poolMock: Pool = mock(Pool);
+    when(poolMock.query(anyString())).thenReturn({ rows: ["testResult"] });
+    const database = instance(poolMock);
+    DumbbellDatabase.buildMockDatabase(database);
+}
