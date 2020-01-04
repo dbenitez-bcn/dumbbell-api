3202a9c273ac5f1105f5f3f900802d2e4d94eada
David Benitez Riba <dbr.bcn@gmail.com>
2020-01-04 19:28:12 +0100

    100% coverage

---
+++ src/services/exercises/routes.test.ts
@@ -0,0 +1,25 @@
+import express, { Router } from "express";
+import request from "supertest";
+import { applyMiddleware, applyRoutes } from "../../utils";
+import middleware from "../../middleware";
+import errorHandlers from "../../middleware/errorHandlers";
+import routes from "./routes";
+
+
+describe("ExercicesController", () => {
+    let router: Router;
+
+    beforeEach(() => {
+        router = express();
+        applyMiddleware(middleware, router);
+        applyRoutes(routes, router);
+        applyMiddleware(errorHandlers, router);
+    });
+
+    it("Should return 'Method not found.' message when wrong endpoint called", async () => {
+        const response = await request(router).get("/imposible-endpoint")
+
+        expect(response.status).toEqual(404);
+        expect(response.text).toEqual("Method not found.");
+    });
+});
--- src/services/exercises/routes.ts
+++ src/services/exercises/routes.ts
@@ -3,14 +3,6 @@ import { getExercisesHandler, createExerciseHandler } from "./exercisesControlle
 
 export default [
   {
-    path: "/",
-    method: "get",
-    handler: async (req: Request, res: Response) => {
-      res.send(`<h2>Welcome to dumbbell API</h2>
-      <b>Exercises</b>: ${process.env.DB_HOST}/exercises`);
-    }
-  },
-  {
     path: "/exercises",
     method: "get",
     handler: getExercisesHandler
