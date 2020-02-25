fee3d50da903f6795a3c550e79ea70333fce72e7
dbenitez-bcn <dbr.bcn@gmail.com>
2020-02-25 23:04:07 +0100

    Changes port

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ applyMiddleware(middleware, router);
 applyRoutes(routes, router);
 applyMiddleware(errorHandlers, router);
 
-const { PORT = 3000 } = process.env;
+const { PORT = 9000 } = process.env;
 const server = http.createServer(router);
 
 server.listen(PORT, () =>
