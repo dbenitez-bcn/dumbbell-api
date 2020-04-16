82bd6f16c7a8405d7ea6843f7ba54aaef34b6fc1
dbenitez-bcn <dbr.bcn@gmail.com>
2020-04-16 21:59:40 +0200

    adds strings in constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -7,6 +7,11 @@ export class Constants {
     public static INVALID_NAME_PARAM = "Invalid name. This param can't be empty";
     public static INVALID_DESCRIPTION_PARAM = "Invalid description. This param can't be empty";
     public static INVALID_DIFFICULTY_PARAM = "Invalid difficulty: This params must be a number between 1 to 10";
+    public static INVALID_USERNAME_LENGTH = "Invalid username. Should have 4 characters or more";
+    public static INVALID_USERNAME_CHARACTERS = "Invalid username. Blank spaces are not valid and just - and _ are allowed";
+    public static INVALID_EMAIL_PARAM = "Invalid email. Should be a real email address";
+    public static INVALID_PASSWORD_LENGTH = "Invalid password. Should have 8 characters or more";
+    public static INVALID_PASSWORD_FORMAT = "Invalid password. Should contain at lest 1 character and 1 number";
 }
 
 export class Endpoints {
