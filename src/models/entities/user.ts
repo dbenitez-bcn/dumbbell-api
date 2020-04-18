9befcab54958fa487b7c06bb06b2be45cc5febc8
dbenitez-bcn <dbr.bcn@gmail.com>
2020-04-18 20:35:01 +0200

    updates users unique constrains

--- src/models/entities/user.ts
+++ src/models/entities/user.ts
@@ -1,7 +1,8 @@
 import { Entity, Unique, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from "typeorm";
 
 @Entity(process.env.DB_TB_USERS)
-@Unique(['username', 'email'])
+@Unique(['email'])
+@Unique(['username'])
 export class User {
     @PrimaryGeneratedColumn('uuid')
     id!: string;
