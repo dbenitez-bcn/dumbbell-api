1db62c3871a41c030b049599a19492650802335a
dbenitez-bcn <dbr.bcn@gmail.com>
2020-04-22 19:29:25 +0200

    adds login method

---
+++ src/services/auth/authController.ts
@@ -0,0 +1,37 @@
+import bycrypt from 'bcryptjs';
+import { Request, Response } from "express";
+import { getRepository } from 'typeorm';
+import { User } from '../../models/entities/user';
+import LoginRequest from '../../models/requests/loginRequest';
+import { parseLoginBody } from './validations';
+import { Constants } from '../../config/constants';
+
+export const login = async (req: Request, res: Response) => {
+    let request: LoginRequest;
+    let user: User;
+
+    try {
+        request = await parseLoginBody(req.body);
+    } catch (error) {
+        res.status(422).send(error.message);
+        return;
+    }
+
+    try {
+        user = await getRepository(User)
+            .findOneOrFail({
+                email: request.email
+            })
+    } catch (error) {
+        res.status(404).send(Constants.LOGIN_FAILURE);
+        return;
+    }
+
+    const isRightPassword = await bycrypt.compare(request.password, user.password);
+    if (isRightPassword) {
+        res.status(200).send();
+    }
+    else {
+        res.status(401).send(Constants.LOGIN_FAILURE);
+    }
+}
