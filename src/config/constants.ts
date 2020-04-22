d5f07b4b90b4414bc09bbcdf6faf80afad2a172b
dbenitez-bcn <dbr.bcn@gmail.com>
2020-04-22 15:51:28 +0200

    Updates constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -12,8 +12,10 @@ export class Constants {
     public static USERNAME_ALREADY_EXIST = "Username already exist";
     public static EMAIL_ALREADY_EXIST = "Email already exist";
     public static INVALID_EMAIL_PARAM = "Invalid email. Should be a real email address";
+    public static MISSING_EMAIL = "Missing email address";
     public static INVALID_PASSWORD_LENGTH = "Invalid password. Should have 8 characters or more";
     public static INVALID_PASSWORD_FORMAT = "Invalid password. Should contain at lest 1 character in capital and 1 number";
+    public static MISSING_PASSWORD = "Missing password";
 }
 
 export class Endpoints {
