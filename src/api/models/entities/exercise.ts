352b64f31eee8795b2d69501efe4fa778a9e72e1
dbenitez-bcn <dbr.bcn@gmail.com>
2020-05-01 14:50:34 +0200

    implements repository

--- src/api/models/entities/exercise.ts
+++ src/api/models/entities/exercise.ts
@@ -3,12 +3,6 @@ import { BaseTableEntity } from './baseTableEntity';
 
 @Entity(process.env.DB_TB_EXERCISES)
 export class Exercise extends BaseTableEntity{
-    constructor (id: number, created_at: string, updated_at: string, name: string, description: string, difficulty: number) {
-        super(id, created_at, updated_at)
-        this.name = name;
-        this.description = description;
-        this.difficulty = difficulty;
-    }
 
     @Column('varchar')
     name: string;
