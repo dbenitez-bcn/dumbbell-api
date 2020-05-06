84741281d92f97c36ea1e57e1e50e2656788fee1
dbenitez-bcn <dbr.bcn@gmail.com>
2020-05-06 22:39:31 +0200

    adds update

--- src/src/exercises/services/exerciseService.ts
+++ src/src/exercises/services/exerciseService.ts
@@ -1,6 +1,10 @@
 import ExerciseRepository from "../domain/repositories/exerciseRepository";
 import Exercise from "../domain/aggregates/exercise";
 import ExerciseId from "../domain/valueObject/exerciseId";
+import ExerciseParams from "../domain/aggregates/exerciseParams";
+import Name from "../domain/valueObject/name";
+import Description from "../domain/valueObject/description";
+import Difficulty from "../domain/valueObject/difficulty";
 
 export default class ExerciseService<T> {
     constructor(private readonly repository: ExerciseRepository<T>) { }
@@ -17,6 +21,15 @@ export default class ExerciseService<T> {
     async getById(id: number) {
         const exerciseId = new ExerciseId(id);
         return await this.repository.getById(exerciseId);
+    }
+
+    async update(id: number, name: string, description: string, difficulty: number) {
+        const exerciseId = new ExerciseId(id);
+        const params: ExerciseParams = {}
+        if (typeof name === 'string') params.name = new Name(name);
+        if (typeof description === 'string') params.description = new Description(description);
+        if (typeof difficulty === 'number') params.difficutly = new Difficulty(difficulty);
+        await this.repository.update(exerciseId, params);
     }
 
     async delete(id: number) {
