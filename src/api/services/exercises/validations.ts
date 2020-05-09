ddf53447102e7b0884f441eb2bf57a4365090913
dbenitez-bcn <dbr.bcn@gmail.com>
2020-05-09 22:11:15 +0200

    removes unused validations

--- src/api/services/exercises/validations.ts
+++
@@ -1,8 +0,0 @@
-import ExerciseCreationRequest from "../../models/requests/exerciseCreationRequest";
-import ExerciseCreationBody from "../../models/bodies/exerciseCreationBody";
-
-export class ExerciseBodyValidator {
-    creationBody(body: ExerciseCreationBody): ExerciseCreationRequest {
-        return new ExerciseCreationRequest(body.name, body.description, body.difficulty);
-    }
-}
