25b49860df520aabbfa384898fd1bc11fa3ae7b7
dbenitez-bcn <dbr.bcn@gmail.com>
2020-05-10 00:26:12 +0200

    cleans constants

--- src/api/config/constants.ts
+++ src/api/config/constants.ts
@@ -2,13 +2,6 @@ export class Constants {
     public static NO_EXERCISES_FOUND = "No exercises found";
     public static USER_NOT_FOUND = "User not found";
     public static LOGIN_FAILURE = "Email or password incorrect. Please try again.";
-    public static MISSING_PARAMS = "Missing params";
-    public static DATABASE_ACCESS_FAILED = "A problem has occurred when accessing to database";
-    public static INVALID_ID = "Invalid id";
-    public static NO_EXERCISE_FOUND = "No exercise found";
-    public static INVALID_NAME_PARAM = "Invalid name. This param can't be empty";
-    public static INVALID_DESCRIPTION_PARAM = "Invalid description. This param can't be empty";
-    public static INVALID_DIFFICULTY_PARAM = "Invalid difficulty: This params must be a number between 1 to 10";
     public static INVALID_USERNAME_LENGTH = "Invalid username. Should have 4 characters or more";
     public static INVALID_USERNAME_CHARACTERS = "Invalid username. Blank spaces are not valid and just - and _ are allowed";
     public static USERNAME_ALREADY_EXIST = "Username already exist";
