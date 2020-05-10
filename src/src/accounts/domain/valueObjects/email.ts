5256306f61c949ddd077d5ecfd83ff4340d2f8ac
dbenitez-bcn <dbr.bcn@gmail.com>
2020-05-10 13:56:01 +0200

    adds email validations

--- src/src/accounts/domain/valueObjects/email.ts
+++ src/src/accounts/domain/valueObjects/email.ts
@@ -1,5 +1,10 @@
+import InvalidEmail from "../errors/invalidEmail";
+
 export default class Email {
     constructor(value: string) {
+        if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value)) {
+            throw new InvalidEmail();
+        }
         this.value = value;
     }
 
