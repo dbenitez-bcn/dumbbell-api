47e5b3149720893049fc9321fd9bdedec4a607c4
dbenitez-bcn <dbr.bcn@gmail.com>
2020-05-10 14:27:07 +0200

    adds constants

--- src/src/accounts/constants/index.ts
+++ src/src/accounts/constants/index.ts
@@ -2,4 +2,6 @@ export default class Constants {
     public static INVALID_USERNAME_LENGTH = "Invalid username. Should have 4 characters or more";
     public static INVALID_USERNAME_FORMAT = "Invalid username. Blank spaces are not valid and just - and _ are allowed";
     public static INVALID_EMAIL = "Invalid email. Should be a real email address";
+    public static INVALID_PASSWORD_LENGTH = "Invalid password. Should have 8 characters or more";
+    public static INVALID_PASSWORD_FORMAT = "Invalid password. Should contain at lest 1 letter and 1 number";
 }
