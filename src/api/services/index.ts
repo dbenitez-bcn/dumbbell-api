3d0e60f334e3289ac1da47c51c7bf70146db5d4a
dbenitez-bcn <dbr.bcn@gmail.com>
2020-05-13 01:20:14 +0200

    renames auth

--- src/api/services/index.ts
+++ src/api/services/index.ts
@@ -1,5 +1,5 @@
 import exercisesRoutes from "./exercises/routes";
 import usersRoutes from "./users/routes";
-import authRoutes from "./auth/routes";
+import authRoutes from "./auth_old/routes";
 
 export default [...exercisesRoutes, ...usersRoutes, ...authRoutes];
