9ffa0dc31e18f7962fa73deba0487232768d8ace
dbenitez-bcn <dbr.bcn@gmail.com>
2020-05-17 16:03:53 +0200

    adds exercises e2e test

--- src/e2e/exercises.spec.ts
+++ src/e2e/exercises.spec.ts
@@ -6,20 +6,60 @@ import { isNumber } from "util";
 describe('Exercises e2e', () => {
     let app: any;
 
+    const exerciseParams = {
+        name: 'Test name',
+        description: 'Test description',
+        difficulty: 5
+    };
+
     beforeAll(async () => {
         app = await startServer();
     })
+
+    describe('Fetch exercises', () => {
+        test('Happy path', async () => {
+            const noExercises = await request(app)
+                .get(Endpoints.EXERCISES)
+                .send();
+
+            expect(noExercises.status).toBe(404);
+
+            await createExerciseAndGetId(exerciseParams);
+
+            const exercisesList = await request(app)
+                .get(Endpoints.EXERCISES)
+                .send();
+
+            expect(exercisesList.status).toBe(200);
+            expect(exercisesList.body.length).toBeGreaterThan(0);
+        })
+    })
+
+    describe('Fetch one exercise', () => {
+        test('Happy path', async () => {
+            const id = await createExerciseAndGetId(exerciseParams);
+            const response = await request(app)
+                .get(Endpoints.EXERCISE + '/' + id)
+                .send();
+
+            expect(response.status).toBe(200);
+            expect(response.body).not.toBeNull();
+        })
+        test('Sad path', async () => {
+            const response = await request(app)
+                .get(Endpoints.EXERCISE + '/1000')
+                .send();
+
+            expect(response.status).toBe(404);
+            expect(response.body).not.toBeNull();
+        })
+    })
+
     describe('Exercise creation', () => {
         test('Happy path', async () => {
-            const params = {
-                name: 'Test name',
-                description: 'Test description',
-                difficulty: 5
-            };
-
             const response = await request(app)
                 .post(Endpoints.EXERCISE)
-                .send(params);
+                .send(exerciseParams);
 
             expect(response.status).toBe(201);
             expect(response.body.name).toBe('Test name');
@@ -27,7 +67,6 @@ describe('Exercises e2e', () => {
             expect(response.body.difficulty).toBe(5);
             expect(isNumber(response.body.id)).toBe(true);
         })
-
         describe('Sad path', () => {
             test('Given an invalid name should send a 422 status', async () => {
                 const params = {
@@ -35,11 +74,11 @@ describe('Exercises e2e', () => {
                     description: 'Test description',
                     difficulty: 5
                 };
-    
+
                 const response = await request(app)
                     .post(Endpoints.EXERCISE)
                     .send(params);
-    
+
                 expect(response.status).toBe(422);
             })
 
@@ -48,11 +87,11 @@ describe('Exercises e2e', () => {
                     name: 'Test name',
                     difficulty: 5
                 };
-    
+
                 const response = await request(app)
                     .post(Endpoints.EXERCISE)
                     .send(params);
-    
+
                 expect(response.status).toBe(422);
             })
 
@@ -62,13 +101,102 @@ describe('Exercises e2e', () => {
                     description: 'Test description',
                     difficulty: 100
                 };
-    
+
                 const response = await request(app)
                     .post(Endpoints.EXERCISE)
                     .send(params);
-    
+
                 expect(response.status).toBe(422);
             })
         })
     })
+
+    describe('Exercise update', () => {
+        let id: number;
+
+        beforeAll(async () => {
+            id = await createExerciseAndGetId(exerciseParams);
+        })
+
+        test('Happy path', async () => {
+            const response = await request(app)
+                .put(Endpoints.EXERCISE + '/' + id)
+                .send(exerciseParams);
+
+            expect(response.status).toBe(204);
+        })
+
+        describe('Sad path', () => {
+            test('Given an invalid name should send a 422 status', async () => {
+                const params = {
+                    name: '',
+                    description: 'Test description',
+                    difficulty: 5
+                };
+
+                const response = await request(app)
+                    .put(Endpoints.EXERCISE + '/' + id)
+                    .send(params);
+
+                expect(response.status).toBe(422);
+            })
+
+            test('Given an invalid description should send a 422 status', async () => {
+                const params = {
+                    name: 'Test name',
+                    description: '',
+                    difficulty: 5
+                };
+
+                const response = await request(app)
+                    .put(Endpoints.EXERCISE + '/' + id)
+                    .send(params);
+
+                expect(response.status).toBe(422);
+            })
+
+            test('Given an invalid difficulty should send a 422 status', async () => {
+                const params = {
+                    name: 'Test name',
+                    description: 'Test description',
+                    difficulty: 100
+                };
+
+                const response = await request(app)
+                    .put(Endpoints.EXERCISE + '/' + id)
+                    .send(params);
+
+                expect(response.status).toBe(422);
+            })
+        })
+    })
+
+    describe('Delete exercise', () => {
+        test('Happy path', async () => {
+            const id = await createExerciseAndGetId(exerciseParams);
+
+            const response = await request(app)
+                .delete(Endpoints.EXERCISE + '/' + id)
+                .send();
+
+            expect(response.status).toBe(204);
+        })
+
+        test('Sad path', async () => {
+            const response = await request(app)
+                .get(Endpoints.EXERCISE + '/1000')
+                .send();
+
+            expect(response.status).toBe(404);
+            expect(response.body).not.toBeNull();
+        })
+    })
+
+    const createExerciseAndGetId = async (params: any): Promise<number> => {
+        const response = await request(app)
+            .post(Endpoints.EXERCISE)
+            .send(params);
+
+        return response.body.id
+    }
 })
