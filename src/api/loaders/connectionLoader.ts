80fd65e745091f6c4489ae6d5314f569f143883e
dbenitez_bcn <dbr.bcn@gmail.com>
2020-05-20 16:19:11 +0200

    ci test

--- src/api/loaders/connectionLoader.ts
+++ src/api/loaders/connectionLoader.ts
@@ -1,11 +1,14 @@
 import { createConnection, ConnectionOptions } from "typeorm";
 import { testConnection } from "../utils/testConnection";
 import { defaultConnection } from "../utils/defaultConnection";
+import { ciConnection } from "../utils/ciConnection";
 
 export const connectionLoader = async (appEnv: string | undefined) => {
   let connection: ConnectionOptions;
   if (appEnv === 'test' || appEnv === undefined) {
     connection = testConnection;
+  } else if (appEnv === 'ci') {
+    connection = ciConnection;
   } else {
     connection = defaultConnection
   }
---
+++ src/api/utils/ciConnection.ts
@@ -0,0 +1,19 @@
+import { ConnectionOptions } from "typeorm";
+import { getDatabaseHost } from "./getDatabaseHost";
+import Secrets from "../config/secrets";
+
+const dbHost = getDatabaseHost(Secrets.APP_ENV);
+
+export const ciConnection: ConnectionOptions = {
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "circleci",
+    password: "Passw0rd",
+    database: 'circleci',
+    entities: ["dist/api/models/entities/**/*.js"],
+    migrations: ["src/api/database/migrations/**/*.ts"],
+    dropSchema: true,
+    synchronize: true,
+    logging: false
+}
