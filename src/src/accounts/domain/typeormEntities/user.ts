59bb574429d991d39c63ddc409015440c5090346
dbenitez-bcn <dbr.bcn@gmail.com>
2020-05-23 14:28:14 +0200

    adds role to user

--- src/src/accounts/domain/typeormEntities/user.ts
+++ src/src/accounts/domain/typeormEntities/user.ts
@@ -1,5 +1,6 @@
 import { Entity, Unique, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from "typeorm";
 import Secrets from "../../../../api/config/secrets";
+import UserRole from "../valueObjects/userRole";
 
 @Entity(Secrets.DB_TB_USERS)
 @Unique(['email'])
@@ -16,6 +17,13 @@ export default class UserDB {
 
     @Column('varchar')
     password: string;
+
+    @Column({
+        type: 'enum',
+        enum: UserRole,
+        default: UserRole.USER
+    })
+    role: UserRole
 
     @CreateDateColumn()
     created_at: Date;
---
+++ src/src/accounts/domain/valueObjects/userRole.ts
@@ -0,0 +1,6 @@
+enum UserRole {
+    USER = 'user',
+    OPERATOR = 'operator'
+}
+
+export default UserRole;
