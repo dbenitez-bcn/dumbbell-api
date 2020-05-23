0cd87fe56cb534a98289d08fefa5ddb3e8adf024
dbenitez-bcn <dbr.bcn@gmail.com>
2020-05-23 14:45:01 +0200

    adds create user with operator role method

--- src/e2e/auth.spec.ts
+++ src/e2e/auth.spec.ts
@@ -1,27 +1,32 @@
 import request from "supertest";
+import { getRepository } from "typeorm";
 import { startServer } from "../api/server";
 import { Endpoints } from "../api/config/constants";
+import UserDB from "../src/accounts/domain/typeormEntities/user";
+import UserRole from "../src/accounts/domain/valueObjects/userRole";
 
 describe('Auth e2e', () => {
-    const AN_EMAIL = 'qwerty@dumbbell.com';
+    const USER_USERNAME = 'qwerty';
+    const USER_EMAIL = 'qwerty@dumbbell.com';
+    const OPERATOR_USERNAME = 'operator';
+    const OPERATOR_EMAIL = 'operator@dumbbell.com';
     const A_PASSWORD = 'password1234';
 
     let app: any;
 
     beforeAll(async () => {
         app = await startServer();
+        await registerUser();
+        await registerOperator();
     })
 
     describe('Users authentication', () => {
-        beforeAll(async () => {
-            await registerUser();
-        })
 
         test('Happy path', async () => {
             const response = await request(app)
             .post(Endpoints.LOGIN)
             .send({
-                email: AN_EMAIL,
+                email: USER_EMAIL,
                 password: A_PASSWORD
             })
 
@@ -46,7 +51,7 @@ describe('Auth e2e', () => {
                 const response = await request(app)
                 .post(Endpoints.LOGIN)
                 .send({
-                    email: AN_EMAIL,
+                    email: USER_EMAIL,
                     password: 'qwerty1234'
                 })
     
@@ -56,13 +61,37 @@ describe('Auth e2e', () => {
         })
     })
 
+    describe('Users authentication in admin panel', ()=> {
+
+    })
+
     const registerUser = async () => {
         await request(app)
         .post(Endpoints.REGISTER)
         .send({
-            username: 'qwerty',
-            email: AN_EMAIL,
+            username: USER_USERNAME,
+            email: USER_EMAIL,
             password: A_PASSWORD
         })
+    }
+
+    const registerOperator = async () => {
+        await request(app)
+        .post(Endpoints.REGISTER)
+        .send({
+            username: OPERATOR_USERNAME,
+            email: OPERATOR_EMAIL,
+            password: A_PASSWORD
+        })
+        await updateRoleToOperator(OPERATOR_USERNAME);
+    }
+
+    const updateRoleToOperator = async (username: string) => {
+        const repo = getRepository(UserDB);
+        const operator = await repo.findOne({
+            username: username
+        });
+        operator!.role = UserRole.OPERATOR;
+        repo.save(operator!);
     }
 })
