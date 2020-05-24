aefedb1a7d5ce589610b09920cefcec6677fe47b
dbenitez-bcn <dbr.bcn@gmail.com>
2020-05-24 23:02:42 +0200

    adds TokenGeneratorService into DIContainer

--- src/api/config/inversify.config.ts
+++ src/api/config/inversify.config.ts
@@ -9,6 +9,7 @@ import ExerciseController from "../services/exercises/exerciseController";
 import ExerciseRepository from "../../src/exercises/domain/repositories/exerciseRepository";
 import TypeormExerciseRepository from "../../src/exercises/infrastructure/typeormExerciseRepository";
 import ExerciseService from "../../src/exercises/services/exerciseService";
+import TokenGeneratorService from "../../src/accounts/application/tokenGeneratorService";
 
 const DIContainer = new Container();
 
@@ -17,6 +18,7 @@ DIContainer.bind<UserRepository>(DITypes.UserRepository).to(TypeormUsersReposito
 DIContainer.bind<ExerciseRepository>(DITypes.ExerciseRepository).to(TypeormExerciseRepository);
 
 // Services
+DIContainer.bind<TokenGeneratorService>(TokenGeneratorService).to(TokenGeneratorService);
 DIContainer.bind<AccountService>(AccountService).to(AccountService);
 DIContainer.bind<ExerciseService>(ExerciseService).to(ExerciseService);
 
