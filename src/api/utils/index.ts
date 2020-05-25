15e4bc7cca6cad57ddc5235a0af93bb205904e77
dbenitez-bcn <dbr.bcn@gmail.com>
2020-05-25 20:05:10 +0200

    removes unused files

--- src/api/utils/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { Wrapper } from "../models/types";
-
-export const applyMiddleware = (middlewareWrappers: Wrapper[], router: Router) => {
-    for (const wrapper of middlewareWrappers) {
-        wrapper(router);
-    }
-};
