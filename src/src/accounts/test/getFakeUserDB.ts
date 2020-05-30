feb00411388cc41eb652ed249d4511cf021f0783
dbenitez-bcn <dbr.bcn@gmail.com>
2020-05-30 21:59:38 +0200

    adds test helpers

---
+++ src/src/accounts/test/getFakeUserDB.ts
@@ -0,0 +1,10 @@
+import UserDB from "../domain/typeormEntities/user"
+
+export const getFakeUserDB = () => {
+    const user = new UserDB();
+    user.username = 'username';
+    user.email = 'user@dumbbell.com';
+    user.password = 'password1234';
+
+    return user;
+}
--- src/src/exercises/test/getFakeExerciseDB.ts
+++ src/src/exercises/test/getFakeExerciseDB.ts
@@ -1,4 +1,5 @@
-import ExerciseDB from "../domain/typeormEntities/exercise"
+import ExerciseDB from "../domain/typeormEntities/exercise";
+import { getFakeUserDB } from "../../accounts/test/getFakeUserDB";
 
 export const getFakeExerciseDB = () => {
     const exercise = new ExerciseDB();
@@ -8,6 +9,7 @@ export const getFakeExerciseDB = () => {
     exercise.difficulty = 10;
     exercise.created_at = new Date().toISOString();
     exercise.updated_at = exercise.created_at;
+    exercise.created_by = getFakeUserDB();
     
     return exercise;
 }
