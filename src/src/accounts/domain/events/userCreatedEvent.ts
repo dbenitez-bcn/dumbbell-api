b076c426ee20b5465fd1671026f80491da32b6e9
dbenitez-bcn <dbr.bcn@gmail.com>
2020-06-28 14:04:22 +0200

    creates UserCreatedEvent event

---
+++ src/src/accounts/domain/events/userCreatedEvent.ts
@@ -0,0 +1,9 @@
+import DumbbellEvent from "../../../../core/domain/events/dumbbellEvent";
+
+export default class UserCreatedEvent extends DumbbellEvent {
+    constructor(
+        public readonly email: string,
+        public readonly username: string,
+        public readonly role: string
+    ) { super(); }
+}
