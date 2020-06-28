977dc7afc580308cd0db7c465ba092017c2adb73
dbenitez-bcn <dbr.bcn@gmail.com>
2020-06-28 14:52:32 +0200

    adds role to fakeUserDB

--- src/src/accounts/test/getFakeUserDB.ts
+++ src/src/accounts/test/getFakeUserDB.ts
@@ -1,10 +1,12 @@
 import UserDB from "../infrastructure/typeorm/entities/user"
+import UserRole from "../domain/valueObjects/userRole";
 
 export const getFakeUserDB = () => {
     const user = new UserDB();
     user.username = 'username';
     user.email = 'user@dumbbell.com';
     user.password = 'password1234';
+    user.role = UserRole.USER;
 
     return user;
 }
